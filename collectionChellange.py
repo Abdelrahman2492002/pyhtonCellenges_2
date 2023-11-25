07cd4d2285a5bf054e6b867c885a429a7e85752b
Abdelrahman2492002 <abdoesmailahmedkhalaph2492002@gmail.com>
2023-11-25 20:15:16 +0200

    End From Collection Solution

---
+++ collectionChellange.py
@@ -0,0 +1,38 @@
+# Collection Chellange
+
+star1  = 'Sol'
+star2  ='Alpha Centauri'
+star3  = 'Barnard'
+star4  = 'Wolf 359'
+
+
+African          =     'Kilimanjaro'
+Atarctic         =     'Vinson'
+Australian       =     'Puncak Jaya'
+Eurasian         =     'Everest'
+North_American   =     'Denali'
+Pacific          =     'Mauna Kea'
+South_American   =     'Aconcagua'
+
+# ========================Collection Chellange Solution=======================
+
+stars = [
+    'Sol',
+    'Alpha Centauri',
+    'Barnard',
+    'Wolf 359',
+]
+
+print(stars[3])
+
+peaks = {
+    'African'          :     'Kilimanjaro',
+    'Atarctic'         :     'Vinson',
+    'Australian'       :     'Puncak Jaya',
+    'Eurasian'         :     'Everest',
+    'North_American'   :     'Denali',
+    'Pacific'          :     'Mauna Kea',
+    'South_American'   :     'Aconcagua',
+}
+
+print(peaks['Pacific'])
