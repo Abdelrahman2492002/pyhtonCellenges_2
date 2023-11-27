72074517cd21e6282f071feff97092ac4c290b5d
Abdelrahman2492002 <abdoesmailahmedkhalaph2492002@gmail.com>
2023-11-27 21:02:54 +0200

    iterration chelenge End

---
+++ iterrationChellange.py
@@ -0,0 +1,24 @@
+#Iterration Chellange
+fruits = [
+    'apple',
+    'bananas',
+    'grapes',
+    'mangos',
+    'nectarines',
+    'pears',
+]
+
+
+#=======================Iterration Chellange Solution===================
+
+print("I love pears")
+
+for fruit in fruits:
+    print(fruit)
+
+print("=============================")
+
+counter = 0
+while fruits[counter]  != 'nectarines':
+    print(fruits[counter])
+    counter += 1
