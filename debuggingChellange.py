747bb18fc07f76d4e345ffdbc645a090ed600873
Abdelrahman2492002 <abdoesmailahmedkhalaph2492002@gmail.com>
2023-11-28 02:50:56 +0200

    Debugging Chellange End

---
+++ debuggingChellange.py
@@ -0,0 +1,27 @@
+# التحدى
+# defplant_recommendation(care1)
+#  if care = 'low':
+#    print('صبار')
+# elif care == 'medium':
+#    print('لبلاب')
+# eif care == 'medium':
+#    print('زهرة الأوركيد')
+
+# plant_rec('low')
+# plant_recommendation(medium)
+# plant_recommendation('high')
+
+# ================الحل====================
+def plant_recommendation(care):
+     if care == 'low':
+         print('صبار')
+    
+     elif care == 'medium':
+         print('لبلاب')
+
+     elif care == 'high':
+         print('زهرة الأوركيد')
+
+plant_recommendation('low')
+plant_recommendation('medium')
+plant_recommendation('high')
